1edee7e7fe8b4fa9d33e0dbf3cc1a438847ace2f
Martin Šebek <sbk.martin@gmail.com>
2022-06-19 01:10:53 +0200

    Updated interaction with user and outputs

--- Exchange Online/SetCalendarPermissions.ps1
+++ Exchange Online/SetCalendarPermissions.ps1
@@ -1,19 +1,29 @@
 # Enviroment settings
-# Choose, which mailbox types you want to setup
-# Set true, if you want to set permissions on this mailbox type, false if not
-$setupUsers = $true;
-$setupRooms = $true;
-$setupEquipment = $true;
-$mailboxTypes;
+# Variable for mailbox types you want to setup
+$mailboxTypes = @();
 
-# Setup the mailboxTypes for filtering
-if ($setupUsers) {$mailboxTypes += "UserMailbox"}
-if ($setupRooms) {$mailboxTypes += "RoomMailbox"}
-if ($setupEquipment) {$mailboxTypes += "EquipmentMailbox"}
+# Ask user to input mailbox type identificators which he want to set up
+Write-Output "Please select, which mailbox type(s) you want to set. `nPossibilities are: `n  ID   Description `n  a)   User Mailbox `n  b)   Room Mailbox `n  c)   Equipment Mailbox `nYou can also use combination of those possibilities. `nFor example: 'ac' is valid input"
+$userSelection = Read-Host "Put in selected mailbox type identificators"
+
+# Resolve user's input
+switch ($userSelection) {
+    {$_.contains("a")} {$mailboxTypes += "UserMailbox"}
+    {$_.contains("b")} {$mailboxTypes += "RoomMailbox"}
+    {$_.contains("c")} {$mailboxTypes += "EquipmentMailbox"}
+    {$_.contains("d")} {$mailboxTypes += "SharedMailbox"}
+    default {Write-Host "You have not selected a mailbox type. Terminating script..." -ForegroundColor Yellow -BackgroundColor Black; exit}
+}
 
 
 # Assign the default organization policy to the mailboxes
 foreach ($user in Get-EXOMailbox -RecipientTypeDetails $mailboxTypes) {
-    $calendar = Get-EXOMailboxFolderStatistics -Identity $user.UserPrincipalName -FolderScope calendar | Where-Object { $_.FolderType -eq "Calendar" }
-    Set-EXOMailboxFolderPermission -Identity $calendar -User Default -AccessRights LimitedDetails
+    $calendar = Get-EXOMailboxFolderStatistics -Identity $user.UserPrincipalName -FolderScope calendar | Where-Object { $_.FolderType -eq "Calendar"}
+    $folder = $user.UserPrincipalName + ":\" + $calendar.Name
+    try {
+        Set-MailboxFolderPermission -Identity $folder -User Default -AccessRights LimitedDetails -ErrorAction Stop -wa 0
+        Write-Host "Finished successfully: $folder" -BackgroundColor black -foregroundColor green;
+    } catch {
+        Write-Host "Error while setting up $folder\: $($error.Exception.message)" -ForegroundColor Red -BackgroundColor black;
+    }
 }
